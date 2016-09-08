248e6bfb1036e42165f7b683a57ec9499211aabc
Dawid van Graan <dawid@polymorph.co.za>
2016-09-08 21:51:06 +0200

    Profile image WIP (lots of work, less progress)

--- Android/app/src/main/java/com/polymorph/hildajoubert/helena20/ui/activity/ProfileActivity.java
+++ Android/app/src/main/java/com/polymorph/hildajoubert/helena20/ui/activity/ProfileActivity.java
@@ -1,8 +1,13 @@
 package com.polymorph.hildajoubert.helena20.ui.activity;
 
+import android.app.Activity;
 import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -12,20 +17,38 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.jakewharton.rxbinding.view.RxView;
 import com.polymorph.hildajoubert.helena20.R;
 
-public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+
+public class ProfileActivity extends BaseActivity {
 
     private static final String TAG = ProfileActivity.class.getSimpleName();
+    private final static int PICK_FROM_GALLERY = 1001;
 
+    @BindView(R.id.userName)
     EditText userName;
+    @BindView(R.id.email)
     EditText email;
-    FloatingActionButton fab;
+    @BindView(R.id.fab)
+    View btnFab;
+    @BindView(R.id.profileImage)
+    CircleImageView profileImage;
 
     Dialog dialog;
 
@@ -33,22 +56,25 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        ButterKnife.bind(this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        userName = (EditText) findViewById(R.id.userName);
-        email = (EditText) findViewById(R.id.email);
-
-        fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(this);
 
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             // Name, email address, and profile photo Url
             userName.setText(user.getDisplayName());
-
             email.setText(user.getEmail());
-            Uri photoUrl = user.getPhotoUrl();
+
+            final Uri photoUrl = user.getPhotoUrl();
+            if (photoUrl != null) {
+                Glide.with(this)
+                        .load(photoUrl)
+                        .fitCenter()
+                        .into(profileImage);
+            }
 
             // The user's ID, unique to the Firebase project. Do NOT use this value to
             // authenticate with your backend server, if you have one. Use
@@ -58,6 +84,8 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     }
 
     private void updateProfile() {
+        showProgressDialog();
+        
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
         UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
@@ -65,7 +93,17 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 //                .setPhotoUri(Uri.parse("https://example.com/jane-q-user/profile.jpg"))
                 .build();
 
-        showProgressDialog();
+
+        profileImage.setDrawingCacheEnabled(true);
+        profileImage.buildDrawingCache();
+        Bitmap bitmap = profileImage.getDrawingCache();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+
+
+
 
         user.updateProfile(profileUpdates)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
@@ -82,8 +120,31 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     }
 
     @Override
-    public void onClick(View view) {
-        updateProfile();
+    public void onStart() {
+        super.onStart();
+
+        Subscription subFab = RxView.clicks(btnFab)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Void>() {
+                    @Override
+                    public void call(Void aVoid) {
+                        updateProfile();
+                    }
+                });
+        subs.add(subFab);
+
+        Subscription subAvatar = RxView.clicks(profileImage)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Void>() {
+                    @Override
+                    public void call(Void aVoid) {
+                        Intent intent = new Intent();
+                        intent.setType("image/*");
+                        intent.setAction(Intent.ACTION_GET_CONTENT);
+                        startActivityForResult(Intent.createChooser(intent, "Select Avatar"), PICK_FROM_GALLERY);
+                    }
+                });
+        subs.add(subAvatar);
     }
 
     private void showProgressDialog() {
@@ -94,5 +155,36 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
     private void hideProgressDialog() {
         dialog.hide();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Request to gallery
+        if (requestCode == PICK_FROM_GALLERY) {
+            if (resultCode == Activity.RESULT_OK) {
+                try {
+                    Glide.with(this)
+                            .load(data.getData())
+                            .fitCenter()
+                            .into(profileImage);
+
+//                    Bitmap bitmap = null;
+//                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
+//
+//                    int height = bitmap.getHeight();
+//                    int width = bitmap.getWidth();
+//
+//                    // Compress Bitmap
+//                    bitmap = Bitmap.createScaledBitmap(bitmap, 640, height / (width / 640), true);
+//
+//                    profileImage.setImageBitmap(bitmap);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
