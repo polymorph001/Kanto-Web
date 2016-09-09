73755863016f86f7921584f1e2adc620fec5f56f
Dawid van Graan <dawid@polymorph.co.za>
2016-09-09 12:24:27 +0200

    Add Splash Screen

---
+++ Android/app/src/main/java/com/polymorph/hildajoubert/helena20/ui/activity/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.polymorph.hildajoubert.helena20.ui.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Observable.just(1)
+                .delay(3, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer integer) {
+                        Intent intent = new Intent(SplashActivity.this, SigninActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+
+    }
+}
